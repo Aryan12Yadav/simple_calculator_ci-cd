1a1ac9339e30d0b8899c8b4481e1f954dc2460d2
Aryan12Yadav <aryanyadav0507wp@gmail.com>
2026-06-14 18:53:03 +0530

    add cors

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.database.database import engine
 from app.database.database import Base
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.calculator import router as calculator_router
 
 Base.metadata.create_all(bind = engine)
@@ -12,7 +12,16 @@ app = FastAPI(
     description="Calculator api using restapi",
     version="1.0.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173'
+    ],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 app.include_router(calculator_router)
 
 @app.get("/")
