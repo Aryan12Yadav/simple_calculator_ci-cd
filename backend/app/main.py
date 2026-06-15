284b1e29a0ecbc7c3e1e03c33147e53911b77d32
Aryan12Yadav <aryanyadav0507wp@gmail.com>
2026-06-16 01:04:43 +0530

    add frontend api

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins = [
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'https://simple-calculator-ci-cd.vercel.app/'
     ],
     allow_credentials = True,
     allow_methods = ["*"],
