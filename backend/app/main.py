c8d16d546d27ce09380c4bce8f17bd7f45dfb28e
Aryan12Yadav <aryanyadav0507wp@gmail.com>
2026-06-16 01:29:16 +0530

    add api2

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,8 @@ app.add_middleware(
     allow_origins = [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        
-        'https://calculator-api-7ntd.onrender.com'
+
+        'https://simple-calculator-ci-cd-5gdd.vercel.app/'
     ],
     allow_credentials = True,
     allow_methods = ["*"],
